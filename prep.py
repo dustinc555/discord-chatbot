47edd4f8f6ab1775e2445b8121d8203c8d5cd0a4
Dustin Cook <dustin.cook@mines.sdsmt.edu>
2019-06-18 15:20:32 -0600

    should be about finished with the projects structure, i am ready to start building the model

--- prep.py
+++ prep.py
@@ -7,8 +7,8 @@
 import toml
 import util
 import glob
-import re
-import pandas
+import yaml
+import pandas as pd
 
 settings = util.loadSettings()
 datasets_dir = settings['directories']['datasets']
@@ -24,14 +24,10 @@ def massage_chatterbot():
 	# combine all conversations
 	for fp in file_paths:
 		df = pd.io.json.json_normalize(yaml.load( open(fp) ))
-		data_arr += df['conversations'][0]
+		data_arr.extend(df['conversations'][0])
 
-	
 	# clean up strings and combine
-	data_preppared = ''.join(sentence.replace('\n', ' ').strip() + '\n' for conv in data_arr for sentence in conv)
-
-	# return 
-	return data_preppared
+	return ''.join(str(sentence).replace('\n', ' ').strip() + '\n' for conversation in data_arr for sentence in conversation)
 
 # event dispatch table for loading datasets
 data_preperation_procedures = {
@@ -47,6 +43,6 @@ for data_set in settings['preperation']['sets']:
 	data_file_content += data_preperation_procedures[data_set]()
 
 #save parsed data
-data_file = open(settings['directories']['training'], "w")
+data_file = open(settings['files']['training'], "w")
 data_file.writelines(data_file_content)
 data_file.close()
--- util.py
+++ util.py
@@ -1,8 +1,14 @@
+# util.py
+# set of utility functions to make things smoother
+# includes: 
+# loadSettings, saveTokenizer, loadTokenizer
+
 import numpy as np
 import os
 import cv2
 import glob
 import toml
+import pickle
 
 # returns settings dictionary
 # must be in same dir as settings.toml to work
@@ -12,21 +18,50 @@ def loadSettings():
 	f.close()
 	return toml.loads(settings_content)
 
-# returns 2 4d numpy array
-# (input, answer)
-def loadTrainingData(pathToTrainingData):
-    x_data = []
+# returns 2 2d numpy arrays
+# (input, answer) of tokens
+# tokenizer: https://keras.io/preprocessing/text/
+# note: a value must be specified for unknown words
+# or else it will be ignored and arrays of incorrect size 
+# returned
+def loadTrainingData(path_to_data, tokenizer):
 
-# converts a text file to a numpy array
-def loadTextFile(path):
-    return
+    raw_text  = open(path_to_data).readlines()
+    oov_token = tokenizer.oov_token
 
-# ignore this
-def loadImageSet(folderPath):
-    x_data = []
-    files  = glob.glob(folderPath + "*.jpg")
+    # creates a map for words to tokens
+    # higher frequency words will have a smaller
+    # token value
+    tokenizer.fit_on_text(raw_text)
+    print("Tokenizer:")
+    print(tokenizer.word_counts)
+    print(tokenizer.document_count)
+    print(tokenizer.word_index)
+    print(tokenizer.word_docs)
 
-    for file in files:
-        image = cv2.imread(file)
-        x_data.append(image)
-    return np.array(x_data)
+    # remove newlines or extra content on sides
+    # and tokenize text
+    # its expected that [::2] is a user question and [1::2] is the bots answer
+    raw_text = [tokenizer.text_to_word_sequence(line.strip()) for line in raw_text]
+
+    # get max length sentence
+    max_sentence_length = len(max(raw_text))
+
+    # pad to max size
+    # todo: possibly set limit for this
+    raw_text = [line + [tokenizer.oov_token] * (max_sentence_length - len(line)) for line in raw_text]
+
+    # split array in two for input/anwers
+    # note: arr[start:stop:step]
+    return (np.array(raw_text[::2]), np.array(raw_text[1::2]))
+
+def loadTokenizer(path):
+    pickle_in = open(path, "rb")
+    tokenizer = pickle.load(pickle_in)
+    pickle_in.close()
+    return tokenizer
+
+def saveTokenizer(path, tokenizer):
+    pickel_out = open(path, , "wb")
+    picke.dump(tokenizer, pickel_out, protocol=pickle.HIGHEST_PROTOCOL)
+    pickel_out.close()
