31e5179cb6d19778c0bfb0ba6dee14abaf1e3c98
dustin <dustin.cook@mines.sdsmt.edu>
2019-07-10 13:09:31 -0600

    the bot is ready enough for me to make the discord half

--- prep.py
+++ prep.py
@@ -13,6 +13,32 @@ import pandas as pd
 settings = util.loadSettings()
 datasets_dir = settings['directories']['datasets']
 
+# parses data in custom folder
+# these are my personal entries
+# they can be added directly with no actual parsing
+def parse_custom():
+    # load files
+    file_paths = glob.glob(datasets_dir + "/custom/*.txt")
+
+    data = ""
+
+    for fp in file_paths:
+        custom_doc = open(fp).read()
+        data += custom_doc
+
+    return data
+
+def massage_nps():
+
+    file_paths = glob.glob(datasets_dir + "/nps_chat/*.xml")
+    data = ""
+
+    for fp in file_paths:
+        custom_doc = open(fp).read()
+        data += custom_doc
+
+    return data
+    return 0
 
 def massage_chatterbot():
 	# load files
@@ -31,18 +57,25 @@ def massage_chatterbot():
 
 # event dispatch table for loading datasets
 data_preperation_procedures = {
-	"chatterbot" : massage_chatterbot
+	    "chatterbot" : massage_chatterbot,
+        "custom" : parse_custom,
+        #"nps" : massage_nps
 }
 
+def run():
 
+    data_file_content = ""
 
-data_file_content = ""
+    # merge all datasets into single source
+    for data_set in settings['preperation']['sets']:
+        data_file_content += data_preperation_procedures[data_set]()
 
-# merge all datasets into single source
-for data_set in settings['preperation']['sets']:
-	data_file_content += data_preperation_procedures[data_set]()
+    # parse out already encountered words
+    lines = data_file_content.splitlines()
 
-#save parsed data
-data_file = open(settings['files']['training'], "w")
-data_file.writelines(data_file_content)
-data_file.close()
+    #save parsed data
+    data_file = open(settings['files']['training'], "w")
+    data_file.writelines(data_file_content)
+    data_file.close()
+
+run()
